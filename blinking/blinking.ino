11da48383ac3545608ec5323046ac9c888856c38
Sun <Sun@Sunwin>
2017-03-26 14:03:20 -0700

    blinking code

---
+++ blinking/blinking.ino
@@ -0,0 +1,27 @@
+const int ledpin = 12;
+const int ledpin11 = 11;
+const int pin=10;
+
+void setup() {
+  pinMode(ledpin, OUTPUT);
+  pinMode(ledpin11, OUTPUT);
+  pinMode(pin,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(ledpin, HIGH);
+ delay(50);
+ digitalWrite(ledpin, LOW);
+ delay(50);
+
+ digitalWrite(ledpin11, HIGH);
+ delay(50);
+ digitalWrite(ledpin11, LOW);
+ delay(50); 
+
+ digitalWrite(pin, HIGH);
+ delay(50);
+ digitalWrite(pin, LOW);
+ delay(50); 
+ 
+}
